cc3df59eef1e4e481c84715c84626360d41aa9c2
zeta96 <ahmad.zainul857@gmail.com>
2019-06-25 19:50:59 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -184,7 +184,7 @@ $home/tools/magiskpolicy --load sepolicy --save $home/ramdisk/sepolicy \
 "allow init sysfs file" \
 "allow init sysfs_graphics file { open write }" \
 "allow thermal-engine shell_exec file { execute }" \
-allow thermal-engine shell_exec file { read open }"\
+"allow thermal-engine shell_exec file { read open }"\
 "allow vendor_init proc_dirty_ratio file write"\
 "allow vendor_init proc_dirty file write" \
 "allow toolbox toolbox capability sys_admin" \
