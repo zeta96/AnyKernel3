2931b7f2ff350ebdc3a284614ea4dbdc7ec41bf9
zeta96 <ahmad.zainul857@gmail.com>
2022-02-10 07:07:22 +0000

    anykernel: fix spectrum bug and add fkm
    * fix dumb selinux

--- anykernel.sh
+++ anykernel.sh
@@ -25,10 +25,9 @@ is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. $home/tools/ak3-core.sh;
+. tools/ak3-core.sh;
 
 
 ## AnyKernel file attributes
@@ -38,12 +37,94 @@ set_perm_recursive 0 0 750 750 $home/ramdisk/init* $home/ramdisk/sbin;
 mount -o remount,rw /;
 mount -o remount,rw /vendor;
 mount -o remount,rw /system;
+mount -o remount,rw /system_root;
 mount -o remount,rw /sbin;
 mount -o remount,rw /data;
 mount -o remount,rw /storage;
 
 ## AnyKernel boot install
 dump_boot;
+
+## Enable Spectrum Support
+ui_print " "
+ui_print "Installing Spectrum & FKM support..."
+
+TMPSYSROOT=/system
+TMPSYSROOT=/system_root
+
+# Locate init rc path
+INIT_FILE1=$TMPSYSROOT/init.rc
+INIT_FILE2=$TMPSYSROOT/system/etc/init/hw/init.rc
+INIT_FILE3=$TMPSYSROOT/system/vendor/etc/init/hw/init.qcom.rc
+
+# Locate init path
+INIT_PATH1=$TMPSYSROOT
+INIT_PATH2=$TMPSYSROOT/system/etc/init
+INIT_PATH3=$TMPSYSROOT/system/vendor/etc/init
+ 
+# Locate selinux file
+SELINUX_FILE1=$TMPSYSROOT/sepolicy
+SELINUX_FILE2=$TMPSYSROOT/system/vendor/etc/selinux/precompiled_sepolicy
+
+if [ -e "$INIT_FILE1" ]; then
+INIT_RC=$INIT_FILE1
+INIT=$INIT_PATH1
+ui_print "- found init folder : $INIT"
+ui_print "- found init file : $INIT_RC"
+remove_line $INIT_RC "import /init.spectrum.rc";
+remove_line $INIT_RC "import /vendor/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /vendor/etc/init/init.spectrum.rc";
+remove_line $INIT_RC "import /system/vendor/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /system/vendor/etc/init/init.spectrum.rc";
+remove_line $INIT_RC "import /system/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /system/etc/init/init.spectrum.rc";
+backup_file $INIT_RC;
+insert_line $INIT_RC "import /system/etc/init/init.spectrum.rc" after "import /init.environ.rc" "import /system/etc/init/init.spectrum.rc";
+insert_line $INIT_RC "import /system/etc/init/init.spectrum.rc" after "import /init.usb.rc" "import /system/etc/init/init.spectrum.rc";
+elif [ -e "$INIT_FILE2" ]; then
+INIT_RC=$INIT_FILE2
+INIT=$INIT_PATH2
+ui_print "- found init folder : $INIT"
+ui_print "- found init file : $INIT_RC"
+remove_line $INIT_RC "import /init.spectrum.rc";
+remove_line $INIT_RC "import /vendor/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /vendor/etc/init/init.spectrum.rc";
+remove_line $INIT_RC "import /system/vendor/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /system/vendor/etc/init/init.spectrum.rc";
+remove_line $INIT_RC "import /system/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /system/etc/init/init.spectrum.rc";
+backup_file $INIT_RC;
+insert_line $INIT_RC "import /system/etc/init/init.spectrum.rc" after "import /init.environ.rc" "import /system/etc/init/init.spectrum.rc";
+insert_line $INIT_RC "import /system/etc/init/init.spectrum.rc" after "import /system/etc/init/hw/init.usb.rc" "import /system/etc/init/init.spectrum.rc";
+elif [ -e "$INIT_FILE3" ]; then
+INIT_RC=$INIT_FILE3
+INIT=$INIT_PATH3
+ui_print "- found init folder : $INIT"
+ui_print "- found init file : $INIT_RC"
+remove_line $INIT_RC "import /init.spectrum.rc";
+remove_line $INIT_RC "import /vendor/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /vendor/etc/init/init.spectrum.rc";
+remove_line $INIT_RC "import /system/vendor/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /system/vendor/etc/init/init.spectrum.rc";
+remove_line $INIT_RC "import /system/etc/init/hw/init.spectrum.rc";
+remove_line $INIT_RC "import /system/etc/init/init.spectrum.rc";
+backup_file $INIT_RC;
+insert_line $INIT_RC "import /system/etc/init/init.spectrum.rc" after "import /vendor/etc/init/hw/init.qcom.usb.rc" "import /system/etc/init/init.spectrum.rc";
+insert_line $INIT_RC "import /system/etc/init/init.spectrum.rc" after "import /vendor/etc/init/hw/init.extras.rc" "import /system/etc/init/init.spectrum.rc";
+else
+	ui_print "!- cannot find init folder or file location, make sure system is not empty."
+fi;
+
+# Remove old spectrum
+if [ -e "$INIT/init.spectrum.rc" ]; then
+	rm -rf $INIT/init.spectrum.rc
+	rm -rf $INIT/init.spectrum.sh
+fi;
+
+if [ -e "$TMPSYSROOT/init.spectrum.rc" ]; then
+	rm -rf $TMPSYSROOT/init.spectrum.rc
+	rm -rf $TMPSYSROOT/init.spectrum.sh
+fi;
 
 # Clean up other kernels' ramdisk files before installing ramdisk
 rm -rf /system/vendor/bin/init.spectrum.rc
@@ -71,6 +152,23 @@ rm -rf /data/media/0/Spectrum/profiles/
 rm -rf /data/media/0/Spectrum/Profiles/
 rm -rf /data/media/0/spectrum/
 rm -rf /data/media/0/Spectrum/
+rm -rf /data/media/0/init.spectrum.rc
+rm -rf /data/media/0/init.spectrum.sh
+rm -rf /data/media/0/init.performance_profiles.rc
+rm -rf /storage/self/primary/init.spectrum.rc
+rm -rf /storage/self/primary/init.spectrum.sh
+rm -rf /storage/self/primary/init.performance_profiles.rc
+rm -rf /storage/self/primary/spectrum/
+rm -rf /storage/self/primary/Spectrum/
+rm -rf /storage/self/primary/Spectrum/profiles/
+rm -rf /storage/self/primary/Spectrum/Profiles/
+rm -rf /storage/self/primary/Spectrum/profiles/balance.profile
+rm -rf /storage/self/primary/Spectrum/profiles/battery.profile
+rm -rf /storage/self/primary/Spectrum/profiles/gaming.profile
+rm -rf /storage/self/primary /Spectrum/profiles/performance.profile
+rm -rf /storage/emulated/0/init.spectrum.rc
+rm -rf /storage/emulated/0/init.spectrum.sh
+rm -rf /storage/emulated/0/init.performance_profiles.rc
 rm -rf /storage/emulated/0/spectrum/
 rm -rf /storage/emulated/0/Spectrum/
 rm -rf /storage/emulated/0/Spectrum/profiles/
@@ -80,55 +178,24 @@ rm -rf /storage/emulated/0/Spectrum/profiles/battery.profile
 rm -rf /storage/emulated/0/Spectrum/profiles/gaming.profile
 rm -rf /storage/emulated/0/Spectrum/profiles/performance.profile
 
-#Spectrum========================================
-cp -rpf $home/ramdisk/init.spectrum.rc /system/vendor/etc/init/init.spectrum.rc
-chmod 644 /system/vendor/etc/init/init.spectrum.rc
-cp -rpf $home/ramdisk/init.spectrum.sh /system/vendor/etc/init/init.spectrum.sh
-chmod 644 /system/vendor/etc/init/init.spectrum.sh
-#spectrum write init.rc only##############################
-if [ -e init.rc ]; then
-	cp -rpf init.rc~ init.rc
-		####for init.qcom.rc
-		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /init.spectrum.rc";
-		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/hw/init.spectrum.rc";
-		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/init.spectrum.rc";
-		remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /system/etc/init/init.spectrum.rc";
-		backup_file /system/vendor/etc/init/hw/init.qcom.rc;
-		#for init.rc
-		remove_line init.rc "import /init.spectrum.rc";
-		remove_line init.rc "import /vendor/etc/init/hw/init.spectrum.rc";
-		remove_line init.rc "import /vendor/etc/init/init.spectrum.rc";
-		remove_line init.rc "import /system/etc/init/init.spectrum.rc";
-		backup_file init.rc;
-		insert_line init.rc "init.spectrum.rc" before "import /init.usb.rc" "import /vendor/etc/init/init.spectrum.rc";
-	else
-		if [ -e /system/vendor/etc/init/hw/init.qcom.rc ]; then
-			cp -rpf /system/vendor/etc/init/hw/init.qcom.rc~  /system/vendor/etc/init/hw/init.qcom.rc
-				remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /init.spectrum.rc";
-				remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/hw/init.spectrum.rc";
-				remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/init.spectrum.rc";
-				remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /system/etc/init/init.spectrum.rc";
-				backup_file /system/vendor/etc/init/hw/init.qcom.rc;
-				insert_line /system/vendor/etc/init/hw/init.qcom.rc "init.spectrum.rc" before "import /vendor/etc/init/hw/init.qcom.usb.rc" "import /vendor/etc/init/init.spectrum.rc";
-		fi;
-fi;
-#Spectrum========================================
+#Spectrum copy
+cp -rpf $home/ramdisk/init.spectrum.rc $INIT/init.spectrum.rc
+chmod 644 $INIT/init.spectrum.rc
+cp -rpf $home/ramdisk/init.spectrum.sh $INIT/init.spectrum.sh
+chmod 644 $INIT/init.spectrum.sh
 
-#remove other file spectrum if any
-rm -rf /system/vendor/etc/init/hw/init.spectrum.rc
-rm -rf /system/vendor/etc/init/hw/init.spectrum.sh
-rm -rf /system/etc/init/init.spectrum.rc
-rm -rf /system/etc/init/init.spectrum.sh
-rm -rf /init.spectrum.rc
-rm -rf /init.spectrum.sh
-
-# fix selinux denials for /init.*.sh
-$home/tools/magiskpolicy --load sepolicy --save $home/ramdisk/sepolicy \
+# Locate selinux file and fix selinux denials for /init.*  with force init to load the monolithic sepolicy at /
+# Method load-split and split sepolicy for treble don't work for now
+if [ -e "$SELINUX_FILE1" ]; then
+SELINUX=$SELINUX_FILE1
+ui_print "- found selinux file : $SELINUX"
+$home/tools/magiskpolicy --load $SELINUX --save $SELINUX \
 "allow init rootfs file execute_no_trans" \
 "allow init sysfs_devices_system_cpu file write" \
 "allow init sysfs_msms_perf file write" \
-"allow init proc file { open write }" \
-"allow init sysfs file" \
+"allow init proc file { open read write}" \
+"allow init sysfs file { open read write }" \
+"allow init sysfs_kgsl lnk_file { open read write}" \
 "allow init vendor_configs_file file execute_no_trans" \
 "allow init su process transition" \
 "allow init su process { siginh rlimitinh }" \
@@ -165,6 +232,67 @@ $home/tools/magiskpolicy --load sepolicy --save $home/ramdisk/sepolicy \
 "allow toolbox default_prop property_service set" \
 "allow toolbox init unix_stream_socket connectto" \
 "allow toolbox init fifo_file { getattr write }"
+elif [ -e "$SELINUX_FILE2" ]; then
+SELINUX=$SELINUX_FILE2
+ui_print "- found selinux file : $SELINUX"
+$home/tools/magiskpolicy --load $SELINUX --save $SELINUX \
+"allow init rootfs file execute_no_trans" \
+"allow init sysfs_devices_system_cpu file write" \
+"allow init sysfs_msms_perf file write" \
+"allow init proc file { open read write}" \
+"allow init sysfs file { open read write }" \
+"allow init sysfs_kgsl lnk_file { open read write}" \
+"allow init vendor_configs_file file execute_no_trans" \
+"allow init su process transition" \
+"allow init su process { siginh rlimitinh }" \
+"allow init sysfs_graphics file { open write }" \
+"allow thermal-engine shell_exec file { read open execute }" \
+"allow thermal-engine shell_exec file execute_no_trans" \
+"allow thermal-engine shell_exec file getattr" \
+"allow thermal-engine thermal-engine capability { sys_resource sys_ptrace }" \
+"allow thermal-engine toolbox_exec file { execute getattr read open }" \
+"allow thermal-engine toolbox_exec file execute_no_trans" \
+"allow thermal-engine vendor_toolbox_exec file execute_no_trans" \
+"allow thermal-engine system_file file execute_no_trans" \
+"allow thermal-engine init dir { search getattr }" \
+"allow thermal-engine kernel dir { search getattr }" \
+"allow thermal-engine kernel file { read open }" \
+"allow thermal-engine init file { read open }" \
+"allow thermal-engine vendor_init file { read open }" \
+"allow vendor_init proc_dirty_ratio file write" \
+"allow vendor_init proc_dirty file write" \
+"allow init init udp_socket ioctl" \
+"allow init init socket read" \
+"allow init object_r chr_file ioctl" \
+"allow hal_graphics_composer_default hal_graphics_composer_default netlink_kobject_uevent_socket read" \
+"allow hal_graphics_composer_default object_r file { read write open }" \
+"allow hal_graphics_composer_default sysfs file { read write open }" \
+"allow untrusted_app object_r chr_file { ioctl open read write }" \
+"allow surfaceflinger object_r chr_file { read write }" \
+"allow priv_app object_r chr_file { getattr ioctl open read write }" \
+"allow platform_app object_r chr_file { getattr ioctl open read write }" \
+"allow untrusted_app vendor_file file { getattr ioctl open read write }" \
+"allow system_server vendor_file file { getattr ioctl open read write }" \
+"allow toolbox toolbox capability sys_admin" \
+"allow toolbox property_socket sock_file write" \
+"allow toolbox default_prop property_service set" \
+"allow toolbox init unix_stream_socket connectto" \
+"allow toolbox init fifo_file { getattr write }" 
+else
+	ui_print "!- cannot find selinux file, make sure system is not empty."
+fi;
+# end selinux 
+
+# check spectrum
+if [ -e "$INIT/init.spectrum.rc" ]; then
+   ui_print "- spectrum/fkm is ok : $INIT/init.spectrum.rc"
+else
+	ui_print "!- installing spectrum/fkm failed"
+fi;
+# end check
+
+ui_print " "
+
 # end ramdisk changes
 
 write_boot;
