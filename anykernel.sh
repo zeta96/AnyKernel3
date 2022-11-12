7e766ce714212c54e3bf3998e545195a24e7dcd5
Chris Renshaw <osm0sis@outlook.com>
2022-11-11 23:42:31 -0400

    Example: clean up some more for readability
    - fix merge

--- anykernel.sh
+++ anykernel.sh
@@ -1,5 +1,5 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
 ### AnyKernel setup
 # begin properties
@@ -19,6 +19,13 @@ supported.versions=
 supported.patchlevels=
 '; } # end properties
 
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
 
 ## boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
@@ -26,15 +33,6 @@ is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
-
-### AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
-
-### AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $home/ramdisk/*;
-set_perm_recursive 0 0 750 750 $home/ramdisk/init* $home/ramdisk/sbin;
 mount -o remount,rw /;
 mount -o remount,rw /vendor;
 mount -o remount,rw /system;
@@ -42,6 +40,9 @@ mount -o remount,rw /system_root;
 mount -o remount,rw /sbin;
 mount -o remount,rw /data;
 mount -o remount,rw /storage;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
@@ -293,8 +294,6 @@ fi;
 # end check
 
 ui_print " "
-
-# end ramdisk changes
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
